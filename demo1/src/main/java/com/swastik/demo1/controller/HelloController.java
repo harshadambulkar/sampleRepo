42d7feac99a8f6248e7cad8f9e3bba0b91b65a9c
Harshad Ambulkar <40738435+harshadambulkar@users.noreply.github.com>
2026-02-03 19:36:02 +0530

    Demo1 project commit

---
+++ demo1/src/main/java/com/swastik/demo1/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.swastik.demo1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        System.out.println("Hello from Demo1 controller 11");
+        return "Hello from Demo1 controller 11";
+    }
+
+}
